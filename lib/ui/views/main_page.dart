b8ec1549b09f8739db127540cf463850f59d7b35
DESKTOP-UH0B8SV\55379 <vmendesneto@gmail.com>
2021-09-30 23:10:51 -0300

    Alteraçoes iniciais, e teste de commit

---
+++ lib/ui/views/main_page.dart
@@ -0,0 +1,41 @@
+import 'package:provider/provider.dart';
+import 'package:re_walls/core/utils/constants.dart';
+import 'package:re_walls/core/viewmodels/carousel_wallpaper_state.dart';
+import 'package:re_walls/core/viewmodels/grid_wallpaper_state.dart';
+import '../widgets/new.dart';
+import '../widgets/popular.dart';
+import 'package:flutter/material.dart';
+
+class MainBody extends StatefulWidget {
+  @override
+  _MainBodyState createState() => _MainBodyState();
+}
+
+class _MainBodyState extends State<MainBody>
+    with AutomaticKeepAliveClientMixin<MainBody> {
+  @override
+  bool get wantKeepAlive => true;
+
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+
+    return ListView(
+      shrinkWrap: true,
+      physics: BouncingScrollPhysics(),
+      children: <Widget>[
+
+        //RETIRANDO UM FILTRO DA HOME
+       /* ChangeNotifierProvider(
+          create: (_) =>
+              CarouselWallpaperState(kdataFetchState.IS_LOADING, null),
+          child: NewWallpapers(),
+        ),*/
+        ChangeNotifierProvider(
+          create: (_) => GridWallpaperState(kdataFetchState.IS_LOADING, null),
+          child: PopularWallpapers(),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/ui/views/settings.dart
@@ -0,0 +1,139 @@
+import 'package:share/share.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:provider/provider.dart';
+import '../../core/utils/dialog_utils.dart';
+import '../../core/utils/theme.dart';
+import '../widgets/card_with_children.dart';
+
+class SettingsPage extends StatefulWidget {
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage>
+    with AutomaticKeepAliveClientMixin<SettingsPage> {
+  @override
+  bool get wantKeepAlive => true;
+  String cacheSize = 'N/A';
+
+  //SEM AS OPÇOES DO DESENVOLVEDOR É DESNECESSARIO
+  /*static const String appUrl =
+          'https://play.google.com/store/apps/details?id=com.bimsina.re_walls',
+      codeUrl = 'https://github.com/bimsina/reWalls',
+      issuesUrl = 'https://github.com/bimsina/reWalls/issues';
+*/
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+    final themeNotifier = Provider.of<ThemeNotifier>(context);
+    final ThemeData state = themeNotifier.getTheme();
+
+    return Container(
+      color: state.primaryColor,
+      child: ListView(
+        physics: BouncingScrollPhysics(),
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        children: <Widget>[
+          CardWithChildren(
+            title: 'Select Theme',
+            children: <Widget>[
+              CustomListTile(
+                title: 'Themes',
+                icon: FontAwesomeIcons.palette,
+                subtitle: 'Select the way you app looks.',
+                onTap: () {
+                  showThemeChangerDialog(context);
+                },
+              ),
+            ],
+          ),
+
+          //DESATIVANDO AS OPÇOES DE DESENVOLVIMENTO
+          //_supportDev(state),
+
+
+        ],
+      ),
+    );
+  }
+
+  //RETIRANDO TELA DE DESENVOLVEDOR
+ /* Widget _supportDev(ThemeData theme) {
+    return CardWithChildren(
+      title: 'Support Development',
+      children: <Widget>[
+        CustomListTile(
+          icon: Icons.share,
+          title: 'Share',
+          subtitle: 'Share this app with your friends.',
+          onTap: () => Share.share(appUrl),
+        ),
+        CustomListTile(
+          icon: FontAwesomeIcons.github,
+          title: 'GitHub',
+          subtitle: 'View the source code on GitHub.',
+          onTap: () => _launchURL(codeUrl),
+        ),
+        CustomListTile(
+          icon: Icons.star,
+          title: 'Rate the app',
+          subtitle: 'Rate the app on Google Play.',
+          onTap: () => _launchURL(appUrl),
+        ),
+        CustomListTile(
+          icon: FontAwesomeIcons.bug,
+          title: 'Report a bug',
+          onTap: () => _launchURL(issuesUrl),
+        ),
+      ],
+    );
+  }*/
+
+  /*_launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }*/
+}
+
+class CustomListTile extends StatelessWidget {
+  final String title, subtitle;
+  final IconData icon;
+  final VoidCallback onTap;
+
+  CustomListTile(
+      {Key key,
+      this.title = 'Title',
+      this.subtitle,
+      this.icon = Icons.star,
+      this.onTap})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final themeNotifier = Provider.of<ThemeNotifier>(context);
+    final ThemeData state = themeNotifier.getTheme();
+
+    return ListTile(
+      onTap: onTap,
+      dense: true,
+      leading: Icon(
+        icon,
+        color: Color(0xff909090),
+      ),
+      title: Text(
+        title,
+        style: state.textTheme.bodyText1,
+      ),
+      subtitle: subtitle == null
+          ? null
+          : Text(
+              subtitle,
+              style: state.textTheme.caption,
+            ),
+    );
+  }
+}
