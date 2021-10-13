729451203f7ff49c1f9bc691e703be1bcc4a6964
Marcus <marckaraujo86@gmail.com>
2021-10-13 13:40:42 -0300

    Add walletconnect for kotlin

--- android/app/src/main/kotlin/com/bimsina/re_walls/MainActivity.kt
+++ android/app/src/main/kotlin/com/bimsina/re_walls/MainActivity.kt
@@ -1,29 +1,41 @@
 package com.bimsina.re_walls
 
+import android.annotation.TargetApi
+import android.app.WallpaperManager
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Build
+import android.view.View
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import org.walletconnect.Session
+import org.walletconnect.nullOnThrow
 import java.io.IOException
-import android.app.WallpaperManager
-import android.graphics.BitmapFactory
-import java.io.File
-import android.os.Build
-import android.annotation.TargetApi
-import android.content.Context
-import androidx.annotation.NonNull
 
 private const val CHANNEL = "com.bimsina.re_walls/wallpaper"
 private const val HOME = "setWallpaper"
 private const val LOCK = "setLockWallpaper"
+private const val WALLET_CONNECTION = "initWalletConnection"
+private const val WALLET_DISCONNECTION = "initWalletDisconnection"
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterActivity(), Session.Callback {
 
   override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
     super.configureFlutterEngine(flutterEngine)
     MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
       call, result ->
       print("call ${call.method}")
-      if (call.method == HOME || call.method == LOCK) {
+      if (call.method == WALLET_CONNECTION) {
+        initWalletConnection()
+      } else if (call.method == WALLET_DISCONNECTION) {
+        initWalletDisconnection()
+      } else if (call.method == HOME || call.method == LOCK) {
         val setWallpaper = setWallpaper(call.arguments as String, applicationContext)
         if (setWallpaper == 0) {
           result.success(setWallpaper)
@@ -50,5 +62,53 @@ class MainActivity: FlutterActivity() {
 
     return setWallpaper
   }
+
+  override fun onStart() {
+    super.onStart()
+    initialSetup()
+  }
+
+  override fun onStatus(status: Session.Status) {
+    when(status) {
+      Session.Status.Approved -> sessionApproved()
+      Session.Status.Closed -> sessionClosed()
+      Session.Status.Connected,
+      Session.Status.Disconnected,
+      is Session.Status.Error -> {
+        // Do Stuff
+      }
+    }
+  }
+
+  private fun initialSetup() {
+    val session = nullOnThrow { ExampleApplication.session } ?: return
+    session.addCallback(this)
+    sessionApproved()
+  }
+
+  override fun onMethodCall(call: Session.MethodCall) {
+  }
+  private fun sessionApproved(): String {
+    val result = runBlocking {
+      "Connected: ${ExampleApplication.session.approvedAccounts()}"
+    }
+    return result
+  }
+
+  private fun sessionClosed() {
+  }
+
+  private fun initWalletConnection() {
+    ExampleApplication.resetSession()
+    ExampleApplication.session.addCallback(this)
+    val i = Intent(Intent.ACTION_VIEW)
+    i.data = Uri.parse(ExampleApplication.config.toWCUri())
+    startActivity(i)
+  }
+
+  private fun initWalletDisconnection() {
+    ExampleApplication.session.kill()
+  }
+
 }
 
