fa898ee5fce7e0e403c252f6a24ecfde50145fd3
Marcus <marckaraujo86@gmail.com>
2021-10-15 12:51:31 -0300

    Add walletconnect for Swift

--- lib/ui/views/conectar.dart
+++ lib/ui/views/conectar.dart
@@ -4,12 +4,6 @@ import 'package:flutter/services.dart';
 
   openMetaMesk() async {
     const platform = const MethodChannel('com.bimsina.re_walls/MainActivity');
-    // try {
-    //   await platform.invokeMethod('initialSetup', null);
-    //   print('Connected....');
-    // } on PlatformException catch (e) {
-    //   print("Failed to initialSetup: '${e.message}'.");
-    // }
     try {
       await platform.invokeMethod('initWalletConnection', null);
       print('Connected....');
