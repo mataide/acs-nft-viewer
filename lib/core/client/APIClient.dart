e3d2b9a932910c8e1309edd8714d450348bca06a
Marcus <marckaraujo86@gmail.com>
2021-10-18 10:46:29 -0300

    Add retrofit_generator

--- lib/core/client/APIClient.dart
+++ lib/core/client/APIClient.dart
@@ -1,7 +1,17 @@
-const String API_BASE_URL = "https://api.etherscan.io/";
-const String API_KEY = "NXIMNR3HSJR58AHXU4TE6Y7ZD73BIHE3MR";
 
-class EndPoints {
+import 'package:retrofit/retrofit.dart';
+import 'package:dio/dio.dart';
+import '../models/eth721.dart';
+
+const String API_BASE_URL = "https://api.etherscan.io/";
+
+
+class APIClient {
+
+  static const String API_KEY = "NXIMNR3HSJR58AHXU4TE6Y7ZD73BIHE3MR";
+
+
+
   static String getPosts(String? subreddit, String filter) {
     return '${API_BASE_URL}r/$subreddit/$filter.json';
   }
@@ -15,6 +25,29 @@ class EndPoints {
     return '${API_BASE_URL}api?module=account&action=tokennfttx&address=$address&page=$page&offset=$offset&startblock=0&endblock=27025780&sort=asc&apikey=$API_KEY';
   }
 }
+
+@RestApi(baseUrl: API_BASE_URL)
+abstract class APIService {
+
+  static const String ENDPOINT_API = "/api";
+
+  factory APIService(Dio dio, {String baseUrl}) = _APIService;
+
+
+  @GET(ENDPOINT_API)
+  Future<_Eth721> getERC721(
+      @Query("module") String module,
+      @Query("action") String action,
+      @Query("address") String address,
+      @Query("page") String page,
+      @Query("offset") String offset,
+      @Query("startblock") String startblock,
+      @Query("endblock") String endblock,
+      @Query("sort") String sort,
+      @Query("apikey") String apikey,
+      );
+}
+
 
 final List<String> kfilterValues = [
   'Hot',
