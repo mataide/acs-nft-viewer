56c624163e2661c58aa85d0ecd6c0df450839f9c
DESKTOP-UH0B8SV\55379 <vmendesneto@gmail.com>
2021-10-19 20:33:16 -0300

    login screen changes

--- lib/ui/views/new_login.dart
+++ lib/ui/views/new_login.dart
@@ -22,64 +22,78 @@ class _LoginPageState extends State<LoginPage> {
   var rest;
   var address;
 
+
+  @override
   Widget _buildChild() {
-    if(rest == null){
-      return ListView(padding: EdgeInsets.all(16.0),
-        children: [
-        Container(
-          child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Expanded(
-            child: TextFormField(
-              controller: _keyCrontollers,
-              style: TextStyle(color: Colors.white),
-              decoration: InputDecoration(
-                hintText: "Insert Public Key",
-                hintStyle: TextStyle(fontSize: 20.0, color: Colors
-                    .white),
-              ),
-            ),
-          ),
-          SizedBox(
-            width: 10.0,
-          ),
-            ElevatedButton(
-            onPressed: () {
-              if(_keyCrontollers.text == "" && rest == null) {
-                final snackBar = SnackBar(content: Text(
-                    'Insert a Public Key !'),
-                  duration: Duration(seconds: 3),);
-                ScaffoldMessenger.of(context).showSnackBar(
-                    snackBar);
-              }else if (rest!= null) {
-                setState(() {
-                });
-              }else{
-                rest = _keyCrontollers.text;
-                setState(() {});
-              }
-              },
-              child: Text(
-                      "OK",
-                    style: TextStyle(fontSize: 16.0),
+    if (rest == null) {
+      final networkStream = _eventChannel
+          .receiveBroadcastStream()
+          .distinct()
+          .map((dynamic event) => event as String);
+      return StreamBuilder<String>(
+          initialData: rest.toString(),
+          stream: networkStream,
+          builder: (context, snapshot) {
+            address = snapshot.data ?? null;
+            print(address);
+            return ListView(padding: EdgeInsets.all(16.0),
+                children: [
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Expanded(
+                          child: TextFormField(
+                            controller: _keyCrontollers,
+                            style: TextStyle(color: Colors.white),
+                            decoration: InputDecoration(
+                              hintText: "Insert Public Key",
+                              hintStyle: TextStyle(fontSize: 20.0, color: Colors
+                                  .white),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10.0,
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                           /* if (_keyCrontollers.text == "" && rest == null) {
+                              final snackBar = SnackBar(content: Text(
+                                  'Insert a Public Key !'),
+                                duration: Duration(seconds: 3),);
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                  snackBar);
+                            } else if (rest != null) {
+                              setState(() {});
+                            } else {*/
+                              address = _keyCrontollers.text;
+                              {_shared();}
+                              setState(() {});
+                            },
+                          //},
+                          child: Text(
+                            "OK",
+                            style: TextStyle(fontSize: 16.0),
+                          ),
+                        ),
+                      ],
                     ),
-              ),
-              ],
-              ),
-              ),
-          SizedBox(height: 20.0,),
-          ElevatedButton(
-          onPressed: () {
-          openMetaMesk();
-          _shared();
-          },
-          child: Text(
-            "Wallet Connect", style: TextStyle(fontSize: 20.0),
-          ),
-          ),
-          ]
-       );
+                  ),
+                  SizedBox(height: 20.0,),
+                  ElevatedButton(
+                    onPressed: () {
+                      openMetaMesk();
+                      {_shared();}
+                    },
+                    child: Text(
+                      "Wallet Connect", style: TextStyle(fontSize: 20.0),
+                    ),
+                  ),
+                ]
+            );
+          }
+            );
     }else{
       return Row(
         crossAxisAlignment: CrossAxisAlignment.center,
@@ -108,20 +122,7 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     final stateData = Provider.of<ThemeNotifier>(context);
     final ThemeData state = stateData.getTheme();
-    final networkStream = _eventChannel
-        .receiveBroadcastStream()
-        .distinct()
-        .map((dynamic event) => event as String);
-
-    return StreamBuilder<String>(
-        initialData: rest,
-        stream: networkStream,
-        builder: (context, snapshot) {
-          print(snapshot.data);
-          address = snapshot.data ?? null;
-          print(address);
-
-          return Scaffold(
+    return Scaffold(
             backgroundColor: state.primaryColor,
             appBar: AppBar(
               title: Text("Acess Wallet"),
@@ -135,12 +136,11 @@ class _LoginPageState extends State<LoginPage> {
             ),
           );
         }
-    );
-  }
   void _shared() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     prefs.setString('key', address);
     rest = prefs.getString('key') ?? '';
+    print(rest);
   }
 }
 
