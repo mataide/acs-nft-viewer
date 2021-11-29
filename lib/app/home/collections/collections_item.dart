bb9f029590583cb47431ee73a951c9908a3a6441
Marcus <marckaraujo86@gmail.com>
2021-11-29 15:59:57 -0300

    Add collapsing Toolbar

--- lib/app/home/collections/collections_item.dart
+++ lib/app/home/collections/collections_item.dart
@@ -22,113 +22,131 @@ class CollectionsItemView extends ConsumerWidget {
     final height = MediaQuery.of(context).size.height;
 
     return Scaffold(
-        appBar: AppBar(
-          iconTheme: state.primaryIconTheme,
-          backgroundColor: Colors.transparent,
-          elevation: 0.0,
-        ),
-        body: SingleChildScrollView(
-            child: Container(
-          decoration: BoxDecoration(
-              gradient: RadialGradient(
-            colors: [Colors.brown, Colors.black54, Colors.black],
-            center: Alignment.topCenter,
-          )),
-          child: Column(
-            children: [
-              SizedBox(height: height * 0.024),
-              SingleChildScrollView(
-                  child: Container(
-                      margin: EdgeInsets.only(
-                          left: (width * 0.02), right: (width * 0.02)),
-                      child: Column(children: [
-                        SizedBox(height: height * 0.008),
-                        Align(
-                            alignment: Alignment.centerLeft,
-                            child: Text(
-                              dataState.collections.tokenName,
-                              style: state.textTheme.caption,
-                            )),
-                        SizedBox(
-                          height: height * 0.01,
-                        ),
-                        Align(
-                            alignment: Alignment.centerLeft,
-                            child: Text(
-                              'Author',
-                              style: state.textTheme.caption,
-                              textAlign: TextAlign.start,
-                            )),
-                        Align(
-                            alignment: Alignment.centerLeft,
-                            child: Text(
-                              "BUSCAR DA API O NOME DO AUTOR",
-                              style: state.textTheme.headline5,
-                              textAlign: TextAlign.start,
-                            )),
-                        SizedBox(
-                          height: height * 0.016,
-                        ),
-                        Align(
-                            alignment: Alignment.centerLeft,
-                            child: Text(
-                              "About",
-                              style: state.textTheme.caption,
-                            )),
-                        Align(
-                            alignment: Alignment.centerLeft,
-                            child: ReadMoreText(
-                              dataState.collections.description,
-                              style: state.textTheme.subtitle1,
-                              trimLines: 4,
-                              colorClickableText: state.buttonColor,
-                              trimMode: TrimMode.Line,
-                              trimCollapsedText: 'Read more',
-                              trimExpandedText: 'Read less',
-                              textAlign: TextAlign.left,
-                              moreStyle: TextStyle(
-                                  fontFamily: 'FuturaPTBold.otf',
-                                  fontWeight: FontWeight.bold,
-                                  color: Color(0xff770955),
-                                  fontSize: 14),
-                              lessStyle: TextStyle(
-                                  fontFamily: 'FuturaPTBold.otf',
-                                  fontWeight: FontWeight.bold,
-                                  color: Color(0xff770955),
-                                  fontSize: 14),
-                              // delimiterStyle: TextStyle(color: Colors.green),
-                              // moreStyle: TextStyle(color: Colors.green),
-                            )),
-                        SizedBox(
-                          height: height * 0.02,
-                        ),
-                        Align(
-                            alignment: Alignment.centerLeft,
-                            child: Text(
-                              'Owned',
-                              style: state.textTheme.caption,
-                            )),
-                        SizedBox(
-                          height: height * 0.01,
-                        ),
-                        _owned(
-                          context,
-                          controller,
-                          dataState,
-                          state,
-                          width,
-                        ),
-                        SizedBox(
-                          height: height * 0.01,
-                        ),
-                        //_more(context, dataState, flagState, state, width),
-                      ])))
-            ],
-          ),
-        )));
+        body: NestedScrollView(
+            headerSliverBuilder:
+                (BuildContext context, bool innerBoxIsScrolled) {
+              return <Widget>[
+                SliverAppBar(
+                  expandedHeight: 200.0,
+                  floating: false,
+                  pinned: true,
+                  flexibleSpace: FlexibleSpaceBar(
+                      centerTitle: true,
+                      title: Text("Collapsing Toolbar",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16.0,
+                          )),
+                      background: Image.network(
+                        "https://images.pexels.com/photos/396547/pexels-photo-396547.jpeg?auto=compress&cs=tinysrgb&h=350",
+                        fit: BoxFit.cover,
+                      )),
+                ),
+              ];
+            },
+            body: SingleChildScrollView(
+                child: Container(
+              decoration: BoxDecoration(
+                  gradient: RadialGradient(
+                colors: [Colors.brown, Colors.black54, Colors.black],
+                center: Alignment.topCenter,
+              )),
+              child: Column(
+                children: [
+                  SizedBox(height: height * 0.024),
+                  SingleChildScrollView(
+                      child: Container(
+                          margin: EdgeInsets.only(
+                              left: (width * 0.02), right: (width * 0.02)),
+                          child: Column(children: [
+                            SizedBox(height: height * 0.008),
+                            Align(
+                                alignment: Alignment.centerLeft,
+                                child: Text(
+                                  dataState.collections.tokenName,
+                                  style: state.textTheme.caption,
+                                )),
+                            SizedBox(
+                              height: height * 0.01,
+                            ),
+                            Align(
+                                alignment: Alignment.centerLeft,
+                                child: Text(
+                                  'Author',
+                                  style: state.textTheme.caption,
+                                  textAlign: TextAlign.start,
+                                )),
+                            Align(
+                                alignment: Alignment.centerLeft,
+                                child: Text(
+                                  "BUSCAR DA API O NOME DO AUTOR",
+                                  style: state.textTheme.headline5,
+                                  textAlign: TextAlign.start,
+                                )),
+                            SizedBox(
+                              height: height * 0.016,
+                            ),
+                            Align(
+                                alignment: Alignment.centerLeft,
+                                child: Text(
+                                  "About",
+                                  style: state.textTheme.caption,
+                                )),
+                            Align(
+                                alignment: Alignment.centerLeft,
+                                child: ReadMoreText(
+                                  dataState.collections.description,
+                                  style: state.textTheme.subtitle1,
+                                  trimLines: 4,
+                                  colorClickableText: state.buttonColor,
+                                  trimMode: TrimMode.Line,
+                                  trimCollapsedText: 'Read more',
+                                  trimExpandedText: 'Read less',
+                                  textAlign: TextAlign.left,
+                                  moreStyle: TextStyle(
+                                      fontFamily: 'FuturaPTBold.otf',
+                                      fontWeight: FontWeight.bold,
+                                      color: Color(0xff770955),
+                                      fontSize: 14),
+                                  lessStyle: TextStyle(
+                                      fontFamily: 'FuturaPTBold.otf',
+                                      fontWeight: FontWeight.bold,
+                                      color: Color(0xff770955),
+                                      fontSize: 14),
+                                  // delimiterStyle: TextStyle(color: Colors.green),
+                                  // moreStyle: TextStyle(color: Colors.green),
+                                )),
+                            SizedBox(
+                              height: height * 0.02,
+                            ),
+                            Align(
+                                alignment: Alignment.centerLeft,
+                                child: Text(
+                                  'Owned',
+                                  style: state.textTheme.caption,
+                                )),
+                            SizedBox(
+                              height: height * 0.01,
+                            ),
+                            _owned(
+                              context,
+                              controller,
+                              dataState,
+                              state,
+                              width,
+                            ),
+                            SizedBox(
+                              height: height * 0.01,
+                            ),
+                            //_more(context, dataState, flagState, state, width),
+                          ])))
+                ],
+              ),
+            ))));
   }
 
-  Widget _owned(context,CollectionsItemController controller, dataState, state, width) {
+  Widget _owned(
+      context, CollectionsItemController controller, dataState, state, width) {
     return Column(children: [
       Row(children: <Widget>[
         Container(
